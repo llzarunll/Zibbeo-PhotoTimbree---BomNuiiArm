74f6d1311b460a014472f56371cccfa1fdb2882a
nuiinuy <nuiinuy@gmail.com>
2016-06-17 03:26:57 +0700

    Update StickerTemplate.java

--- app/src/main/java/com/zibbeo/phototrimbree/Database/StickerTemplate.java
+++ app/src/main/java/com/zibbeo/phototrimbree/Database/StickerTemplate.java
@@ -4,14 +4,39 @@ package com.zibbeo.phototrimbree.Database;
  * Created by Nuii on 6/6/2559.
  */
 public class StickerTemplate {
-    private String sticker_array;
+    private String stickerID;
+    private  float x,y;
+    byte[] sticker ;
     public StickerTemplate() {
     }
 
-    public StickerTemplate(String ssticker_array){
-        this.sticker_array = ssticker_array;
+    public StickerTemplate(String stickerID,byte[] ssticker,Float sx,float sy){
+        this.stickerID = stickerID;
+        this.sticker = ssticker;
+        this.x = sx;
+        this.y = sy;
+    }
+    public StickerTemplate(byte[] ssticker,Float sx,float sy){
+        this.sticker = ssticker;
+        this.x = sx;
+        this.y = sy;
+    }
+    public void setStickerID(String stickerID) {this.stickerID = stickerID;    }
+
+    public void setSticker(byte[] sticker) {
+        this.sticker = sticker;
     }
 
-    public void setSticker(String sticker_array) {this.sticker_array = sticker_array;    }
-    public String getSticker() {return sticker_array;}
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public String getStickerID() {return stickerID;}
+    public byte[] getSticker() {return sticker;}
+    public Float getX() {return x;}
+    public Float getY() {return y;}
 }
