a1d5c2da9dd830e554b5dc84cdefb58238c47f08
nuiinuy <nuiinuy@gmail.com>
2016-06-17 03:25:53 +0700

    Update ImageComposer.java

--- app/src/main/java/com/zibbeo/phototrimbree/Database/ImageComposer.java
+++ app/src/main/java/com/zibbeo/phototrimbree/Database/ImageComposer.java
@@ -4,19 +4,25 @@ package com.zibbeo.phototrimbree.Database;
  * Created by Nuii on 6/6/2559.
  */
 public class ImageComposer {
-    private String id,templateModel,stickerModel;
+    private String id,templateModel,stickerModel1,stickerModel2,stickerModel3,stickerModel4;
 
     public ImageComposer(){}
 
-    public ImageComposer(String sid,String sTemplateModele,String sStickerModel){
+    public ImageComposer(String sid,String sTemplateModel,String sStickerModel1,String sStickerModel2,String sStickerModel3,String sStickerModel4){
         this.id = sid;
-        this.templateModel = sTemplateModele;
-        this.stickerModel = sStickerModel;
+        this.templateModel = sTemplateModel;
+        this.stickerModel1 = sStickerModel1;
+        this.stickerModel2 = sStickerModel2;
+        this.stickerModel3 = sStickerModel3;
+        this.stickerModel4 = sStickerModel4;
     }
 
-    public ImageComposer(String sTemplateModele,String sStickerModel){
-        this.templateModel = sTemplateModele;
-        this.stickerModel = sStickerModel;
+    public ImageComposer(String sTemplateModel,String sStickerModel1,String sStickerModel2,String sStickerModel3,String sStickerModel4){
+        this.templateModel = sTemplateModel;
+        this.stickerModel1 = sStickerModel1;
+        this.stickerModel2 = sStickerModel2;
+        this.stickerModel3 = sStickerModel3;
+        this.stickerModel4 = sStickerModel4;
     }
     public void setImageComposerID(String id) {
         this.id = id;
@@ -26,14 +32,34 @@ public class ImageComposer {
         this.templateModel = templateModel;
     }
 
-    public void setSticker(String stickerModel) {
-        this.stickerModel = stickerModel;
+    public void setSticker1(String stickerModel1) {
+        this.stickerModel1 = stickerModel1;
+    }
+
+    public void setSticker2(String stickerModel2) {
+        this.stickerModel2 = stickerModel2;
+    }
+
+    public void setSticker3(String stickerModel3) {
+        this.stickerModel3 = stickerModel3;
+    }
+
+    public void setSticker4(String stickerModel4) {
+        this.stickerModel4 = stickerModel4;
     }
 
     public String getImageComposerID() { return id;}
     public String getTemplate() { return templateModel; }
-    public String getSticker() {
-        return stickerModel;
+    public String getSticker1() {
+        return stickerModel1;
     }
-
+    public String getSticker2() {
+        return stickerModel2;
+    }
+    public String getSticker3() {
+        return stickerModel3;
+    }
+    public String getSticker4() {
+        return stickerModel4;
+    }
 }
